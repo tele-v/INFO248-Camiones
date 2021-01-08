0f90e23f18d576428aaea47fcb43d54980823481
Sebastián Sanhueza <stereo.telev@gmail.com>
2021-01-07 22:50:17 -0300

    push update recomendacion profes
    redirigiendo el enfoque de desarrollo en torno a casos de uso por recomendación del profesor respecto a la metodología
    ui básica para uso de login
    routing para login (/login) y landing page (/home)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
+  res.render('login');
+});
+
+router.get('/home', (req, res)=> {
   res.render('home');
 });
 
